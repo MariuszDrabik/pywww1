4f29564fd877ce53871bd8ec98cf3473d2b27fbc
Mariusz Dell <drabik.mariusz@gmail.com>
2022-01-05 20:50:42 +0100

    comment

--- pywww/pywww/settings.py
+++ pywww/pywww/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'static_dirs'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static', 'static_dirs'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
